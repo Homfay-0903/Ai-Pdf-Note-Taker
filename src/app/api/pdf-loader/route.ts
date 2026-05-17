0c8d7735cf942d5db26ec9ae53c1aa8a17092557
Homfay-0903 <2790165595@qq.com>
2026-05-17 19:52:54 +0800

    Ver-1.5

--- src/app/api/pdf-loader/route.ts
+++ src/app/api/pdf-loader/route.ts
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
 import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
-import pdfjs from 'pdfjs-dist';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const pdfUrl = 'https://curious-lemur-430.convex.cloud/api/storage/21eb95e8-7607-4cd9-8752-94a7910148d1'
-export async function GET(req) {
+
+export async function GET(req: Request) {
+    //step1
     const response = await fetch(pdfUrl)
     const data = await response.blob()
     const loader = new WebPDFLoader(data)
     const docs = await loader.load()
 
-    let pdfContext = ''
+    let pdfTextContent = ''
     docs.forEach(doc => {
-        pdfContext += doc.pageContent
+        pdfTextContent += doc.pageContent
     })
 
-    return NextResponse.json({ result: pdfContext })
+    //step2
+    const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 100, chunkOverlap: 20 })
+    const output = await splitter.createDocuments([pdfTextContent])
+
+    let splitterList: string[] = []
+    output.forEach(doc => {
+        splitterList.push(doc.pageContent)
+    })
+    return NextResponse.json({ result: splitterList })
 }
